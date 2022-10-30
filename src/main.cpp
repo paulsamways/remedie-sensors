d6e1c0b197317f67ad7c90e7db4729cf2f233c31
Paul Samways <paul@samways.id.au>
2022-10-30 16:16:14 +1100

    Updated pins

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,46 @@
 #include <CAN.h>
 #include <arduino-timer.h>
 
-#define PIN_ENGINE_OIL PIN_A2
-#define PIN_TRANSMISSION_FLUID PIN_A2
-#define PIN_DRIVER_SWITCH_A 2
-#define PIN_DRIVER_SWITCH_B 3
+#define PIN_ENGINE_OIL_TEMPERATURE PIN_A1
+#define PIN_TRANSMISSION_FLUID_TEMPERATURE PIN_A2
+#define PIN_ENGINE_OIL_PRESSURE PIN_A3
+#define PIN_DRIVER_SWITCH_A 5
+#define PIN_DRIVER_SWITCH_B 6
 
-#define CAN_ID_ENGINE_OIL 0x24
-#define CAN_ID_TRANSMISSION_FLUID 0x25
-#define CAN_ID_DRIVER_ID 0x26
+#define CAN_ID_ENGINE_OIL_TEMPERATURE 0x24
+#define CAN_ID_TRANSMISSION_FLUID_TEMPERATURE 0x25
+#define CAN_ID_ENGINE_OIL_PRESSURE 0x26
+#define CAN_ID_DRIVER_ID 0x27
 
 auto timer = timer_create_default();
 
+
+/*
+
+                 ┌┐
+                 ││
+                 ││ Bosch NTC M12
+               ┌─┴┴─┐
+               │ R1 │
+               └┬──┬┘
+                │  │
+                │  │
+                │  │
+                │  │
+                │  │
+                │ ┌┼──────┐
+                │ ││R2    │
+                │ └┤      │
+                │  │      │
+            ┌───┴──┴──────┴────┐
+            │   V  G      A0   │
+            │                  │
+            └──────────────────┘
+            
+*/
+
 double read_bosch_ntc_thermistor(uint8_t pin, int r2) {
-  const double vref = 5.0;
+  const double vref = 3.3;
 
   const double A = 0.0013017331448854658;
   const double B = 0.00025873055639535946;
@@ -32,20 +59,59 @@ double read_bosch_ntc_thermistor(uint8_t pin, int r2) {
   return k - 273.15;
 }
 
+double read_bosch_pst_f1_ntc(uint8_t pin, int r2) {
+  const double vref = 3.3;
+
+  const double A = 1.288158971e-3;
+  const double B = 2.617143934e-4;
+  const double C = 1.624978584e-7;
+
+  int value = analogRead(pin);
+
+  double v = (vref / 1024) * value;
+  double r1 = ((vref * r2) / v) - r2;
+
+  // Steinhart and Hart Equation
+  double logR1 = log(r1);
+  double k = 1 / (A + B * logR1 + C * logR1 * logR1 * logR1);
+  return k - 273.15;
+}
+double read_bosch_pst_f1_pressure(uint8_t pin) {
+  const double vref = 3.3;
+
+  int value = analogRead(pin);
+
+  double v = ((vref / 1024) * value) - 0.33;
+  double p = (145 / 3) * v;
+  return p;
+}
+
 void write_can_packet(int id, uint8_t v) {
-  CAN.beginPacket(id);
-  CAN.write(v);
-  CAN.endPacket();
+  if (CAN.available()) {
+    CAN.beginPacket(id);
+    CAN.write(v);
+    CAN.endPacket();
+  }
+
+  if (Serial.availableForWrite()) {
+    Serial.print(id);
+    Serial.print(": ");
+    Serial.println(v);
+  }
 
   digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
 }
 
-bool engine_oil_temperature(void *) {
+bool engine_oil_temperature_pressure(void *) {
   const double R2 = 10000;
 
-  uint8_t c = round(read_bosch_ntc_thermistor(PIN_ENGINE_OIL, R2));
+  uint8_t c = round(read_bosch_pst_f1_ntc(PIN_ENGINE_OIL_TEMPERATURE, R2));
 
-  write_can_packet(CAN_ID_ENGINE_OIL, c);
+  write_can_packet(CAN_ID_ENGINE_OIL_TEMPERATURE, c);
+
+  double p = round(read_bosch_pst_f1_pressure(PIN_ENGINE_OIL_PRESSURE));
+  
+  write_can_packet(CAN_ID_ENGINE_OIL_PRESSURE, p);
 
   return true;
 }
@@ -53,9 +119,9 @@ bool engine_oil_temperature(void *) {
 bool transmission_fluid_temperature(void *) {
   const double R2 = 10000;
 
-  uint8_t c = round(read_bosch_ntc_thermistor(PIN_TRANSMISSION_FLUID, R2));
+  uint8_t c = round(read_bosch_ntc_thermistor(PIN_TRANSMISSION_FLUID_TEMPERATURE, R2));
 
-  write_can_packet(CAN_ID_TRANSMISSION_FLUID, c);
+  write_can_packet(CAN_ID_TRANSMISSION_FLUID_TEMPERATURE, c);
 
   return true;
 }
@@ -65,23 +131,18 @@ bool driver_id(void *) {
 
   uint8_t result = 0;
 
-  if (digitalRead(PIN_DRIVER_SWITCH_A)) {
+  if (!digitalRead(PIN_DRIVER_SWITCH_A)) {
     result |= 1 << 0;
   }
-  if (digitalRead(PIN_DRIVER_SWITCH_B)) {
+  if (!digitalRead(PIN_DRIVER_SWITCH_B)) {
     result |= 1 << 1;
   }
 
-  for (uint8_t i = 0; i < 8; i += 2) {
-    uint8_t p = (last_driver_id >> i) & 0b00000011;
-
-    if (p != result) {
-      last_driver_id |= result << i;
-      return;
-    }
+  if (last_driver_id != result) {
+    last_driver_id = result;
+  } else {  
+    write_can_packet(CAN_ID_DRIVER_ID, result);
   }
-
-  write_can_packet(CAN_ID_DRIVER_ID, result);
   
   return true;
 }
@@ -90,21 +151,23 @@ void setup() {
   Serial.begin(115200);
 
   // start the CAN bus at 500 kbps
-  while (!CAN.begin(500E3)) {
+  uint8_t attempts = 0;
+  while (!CAN.begin(500E3) && attempts++ < 5) {
     Serial.println("Starting CAN failed!");
     delay(1000);
   }
 
   pinMode(LED_BUILTIN, OUTPUT);
 
-  pinMode(PIN_ENGINE_OIL, INPUT);
-  pinMode(PIN_TRANSMISSION_FLUID, INPUT);
-  pinMode(PIN_DRIVER_SWITCH_A, INPUT);
-  pinMode(PIN_DRIVER_SWITCH_B, INPUT);
+  pinMode(PIN_ENGINE_OIL_TEMPERATURE, INPUT);
+  pinMode(PIN_TRANSMISSION_FLUID_TEMPERATURE, INPUT);
+  pinMode(PIN_ENGINE_OIL_PRESSURE, INPUT);
+  pinMode(PIN_DRIVER_SWITCH_A, INPUT_PULLUP);
+  pinMode(PIN_DRIVER_SWITCH_B, INPUT_PULLUP);
   
-  timer.every(1000, engine_oil_temperature);
+  timer.every(1000, engine_oil_temperature_pressure);
   timer.every(1000, transmission_fluid_temperature);
-  timer.every(250, driver_id);
+  timer.every(1000, driver_id);
 }
 
 void loop() {
